99d7c4ad6bbd6b4b9d32bb4575180863a54e88a9
San4yz <a.pleskov@gmail.com>
2026-01-08 13:54:50 +0200

    Update payments.py

--- payments.py
+++ payments.py
@@ -1,1 +1,13 @@
+import stripe
+from config import STRIPE_API_KEY, PRICE_ID
 
+stripe.api_key = STRIPE_API_KEY
+
+def create_payment_link():
+    session = stripe.checkout.Session.create(
+        mode="subscription",
+        line_items=[{"price": PRICE_ID, "quantity": 1}],
+        success_url="https://t.me/AutoFine_EU_Bot",
+        cancel_url="https://t.me/AutoFine_EU_Bot",
+    )
+    return session.url
